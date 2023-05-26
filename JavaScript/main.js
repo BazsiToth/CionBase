5be2168e0dbcb929c39f3232fd28cc1882fd3d82
Tóth Fazekas Balázs <95696767+QayaKraH@users.noreply.github.com>
2023-05-26 15:34:33 +0200

    .
    .

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -4,7 +4,6 @@ const Tagbitcoin = document.getElementById('btc-price')
 pricesWsbitcoin.onmessage = function (msg) {
     const price = JSON.parse(msg.data).bitcoin;
     Tagbitcoin.textContent = price + " $"
-    console.log(price);
 }
 
 const pricesWsethereum = new WebSocket('wss://ws.coincap.io/prices?assets=ethereum')
@@ -13,5 +12,12 @@ const Tagethereum = document.getElementById('eth-price')
 pricesWsethereum.onmessage = function (msg) {
     const price = JSON.parse(msg.data).ethereum;
     Tagethereum.textContent = price + " $"
-    console.log(price);
+}
+
+const pricesWsRipple = new WebSocket('wss://ws.coincap.io/prices?assets=ripple')
+const TagRipple = document.getElementById('tet-price')
+
+pricesWsRipple.onmessage = function (msg) {
+    const price = JSON.parse(msg.data).ripple;
+    TagRipple.textContent = price + " $"
 }
